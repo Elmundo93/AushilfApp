cab6b1553524b802bb5f8086bc4a6f0c919ae166
Lemont-Kim Mrutschock <lemont@MacBook-Air-von-Lemont-Kim.fritz.box>
2025-05-14 22:53:03 +0200

    ja

--- components/utils/redirect.ts
+++ components/utils/redirect.ts
@@ -1,13 +1,7 @@
 import * as AuthSession from 'expo-auth-session';
-import Constants from 'expo-constants';
 
 export const getRedirectUri = () => {
-  const isDev =
-    Constants.appOwnership === 'expo' ||
-    (Constants.executionEnvironment === 'standalone' && __DEV__);
-
   return AuthSession.makeRedirectUri({
-    useProxy: isDev,
-    scheme: isDev ? undefined : 'myapp',
-  } as any); // 👈 TS meckert nicht mehr
+    useProxy: true,
+  }as any );
 };
